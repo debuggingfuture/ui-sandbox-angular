3ce58629b497eaee4199e7936c30969f18a47dfa
vincentlaucy <lauchunyin@gmail.com>
2016-03-20 14:55:59 +0800

    setup browserstack

--- README.md
+++ README.md
@@ -14,9 +14,30 @@ If you want run only some tests matching a given pattern you can do this in the 
 karma start &
 karma run -- --grep=<pattern>
 
-### Browser
+## Integration test at Browserstack
+TODO
+https://github.com/browserstack/karma-browserstack-example
 
-- Start the server
+### Browser test
+There are 3 modes of testing
+1) local 2) browserstack local 3) browserstack cloud
+
+#### Browserstack Local
+- Use the binary at test/browser/BrowserStackLocal or Install the [binary](https://www.browserstack.com/local-testing)
+1. run `./BrowserStackLocal_Mac <KEY>`
+2. track at https://www.browserstack.com/automate
+
+### Local
+1. Start the server
 webdriver-manager update --standalone
-- Run Test Case
- protractor protractor.conf.js
+
+### Browserstack cloud
+1. Run directly as long as keys are in .env file
+
+
+## Run Test Case
+BROWSER_TEST_MODE=<<mode>> protractor protractor.conf.js
+mode = local / bs_local / bs
+
+## Sample Ref:
+https://github.com/browserstack/protractor-browserstack
--- test/browser/protractor.conf.js
+++ test/browser/protractor.conf.js
@@ -1,4 +1,59 @@
-exports.config = {
-  seleniumAddress: 'http://127.94.0.1:4444/wd/hub',
+require('dotenv').config({path:"../../.env"});
+var _ = require('lodash');
+var localConfig = {
+  "protractor": {
+    seleniumAddress: 'http://127.94.0.1:4444/wd/hub',
+    capabilities: {
+      'browserName': 'chrome',
+      //      'browserName': ['chrome','firefox']
+      'browserstack.debug':true, //tested not much overhead
+      // Needed for testing localhost
+      'browserstack.local': 'true'
+    },
+    jasmineNodeOpts: {
+      defaultTimeoutInterval: 30000
+    }
+  },
+  "application":{
+    "url":""
+  }
+}
+var bsConfig = {
+  "protractor": {
+    seleniumAddress: 'http://hub.browserstack.com/wd/hub',
+    capabilities: {
+      'browserName': 'chrome',
+      //      'browserName': ['chrome','firefox']
+      'browserstack.user': process.env.BROWSER_STACK_USER,
+      'browserstack.key': process.env.BROWSER_STACK_KEY,
+      'browserstack.debug':true //tested not much overhead
+      // Needed for testing localhost
+      // 'browserstack.local': 'true'
+    },
+    jasmineNodeOpts: {
+      defaultTimeoutInterval: 30000
+    }
+  },
+  "application":{
+    "url":""
+  }
+}
+
+var config = {
   specs: ['*.js']
-};
+}
+var mode = process.env.BROWSER_TEST_MODE
+
+if(!mode){
+  console.error('specify in env variable BROWSER_TEST_MODE as local or bs_local or bs')
+}
+console.log("Using Mode:"+mode);
+if(mode === "bs_local"){
+  _.assign(config,bsConfig["protractor"])
+  config["capabilities"]["browserstack.local"]=true;
+}else if (mode === "bs"){
+  _.assign(config,bsConfig["protractor"])
+}else {
+  _.assign(config,localConfig)
+}
+exports.config = config
